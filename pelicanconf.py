96b73743c481a408f7926ac61c313b86c9929a58
Sam Bull <git@sambull.org>
2025-12-23 18:26:23 +0000

    Initialise site

---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+AUTHOR = 'aio-libs'
+SITENAME = 'aio-libs'
+SITEURL = ""
+SUMMARY_MAX_PARAGRAPHS = 1
+
+PATH = "content"
+ARTICLE_PATHS = ["news"]
+
+TIMEZONE = 'UTC'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
