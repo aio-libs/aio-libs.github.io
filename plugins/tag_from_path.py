3325a83587e5db98697d7bb4ac7e574fc43058ce
Sam Bull <git@sambull.org>
2026-02-11 02:41:04 +0000

    Update tag_from_path.py

--- plugins/tag_from_path.py
+++ plugins/tag_from_path.py
@@ -1,13 +1,12 @@
+from typing import Any
+
 from pelican import signals
-from pelican.contents import Article
 from pelican.generators import ArticlesGenerator
 
 
-def tag_from_path(article_generator: ArticlesGenerator, content: Article) -> None:
-    if not hasattr(content, "tags"):
-        content.tags = []
-    content.tags.append(content.tag)
+def tag_from_path(article_generator: ArticlesGenerator, metadata: dict[str, Any]) -> None:
+    metadata.setdefault("tags", []).append(metadata["tag"])
 
 
 def register() -> None:
-    signals.article_generator_write_article.connect(tag_from_path)
+    signals.article_generator_context.connect(tag_from_path)
