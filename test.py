536bdc24de8ef1a953af4c4fce544a79f7ec1406
Sam Bull <git@sambull.org>
2026-02-11 03:14:38 +0000

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ NEWS = Path(__file__).parent / "content" / "news"
 for root, dirs, files in NEWS.walk():
     for name in files:
         f = root / name
-        m = re.search(r"^Tags: (?P<tags>.*)$", f.read_text())
+        m = re.search(r"^Tags: (?P<tags>.*)$", f.read_text(), re.MULTILINE)
         if m and f.parent.name not in m.group("tags"):
             print(f"Tag from path must be repeated in Tags metadata: {f}")
             sys.exit(1)
