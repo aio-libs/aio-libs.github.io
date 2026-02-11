adf93c3049abd264c2d75e64bbdc5f914a8f97b4
Sam Bull <git@sambull.org>
2026-02-11 02:10:50 +0000

    Update tag_from_path.py

--- plugins/tag_from_path.py
+++ plugins/tag_from_path.py
@@ -1,6 +1,6 @@
 from pelican import signals
 from pelican.contents import Article
-from pelican.generator import ArticlesGenerator
+from pelican.generators import ArticlesGenerator
 
 
 def tag_from_path(article_generator: ArticlesGenerator, content: Article) -> None:
