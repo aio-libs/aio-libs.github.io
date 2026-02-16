801c90ef143288fce2feb5290e8cd43d16998bc7
Sam Bull <git@sambull.org>
2026-02-16 21:00:17 +0000

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ for root, dirs, files in NEWS.walk():
     for name in files:
         f = root / name
         m = re.search(r"^Tags: (?P<tags>.*)$", f.read_text(), re.MULTILINE)
+        # Tags header overrides the path tag, so enforce that the path tag is
+        # included in the header for consistency.
         if m and f.parent.name not in m.group("tags"):
             print(f"Tag from path must be repeated in Tags metadata: {f}")
             sys.exit(1)
